32d1bbf7244be08dc218595a4ba6335511628a69
Ferrevdv <ferre.vieren@gmail.com>
2024-07-26 10:57:01 +0200

    ifndef

--- Payload_Type/ceos/ceos/agent_code/ceos/include/Shell.h
+++ Payload_Type/ceos/ceos/agent_code/ceos/include/Shell.h
@@ -1,7 +1,11 @@
 #pragma once
+
+#ifndef SHELL_H
+#define SHELL_H
 #include <windows.h>
 #include "Package.h"
 #include "Parser.h"
 #include "Command.h"
 
 PPackage executeShell(PParser arguments);
+#endif
