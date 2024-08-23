55b9bb4a9c9292fa75f983a8c824ef10f946ba6d
Ferrevdv <ferre.vieren@gmail.com>
2024-08-23 16:19:42 +0200

    windows doc StringCch usage

--- Payload_Type/ceos/ceos/agent_code/ceos/include/Shell.h
+++ Payload_Type/ceos/ceos/agent_code/ceos/include/Shell.h
@@ -3,6 +3,9 @@
 #ifndef SHELL_H
 #define SHELL_H
 #include <windows.h>
+#include <tchar.h> 
+#include <stdio.h>
+#include <strsafe.h>
 #include "Package.h"
 #include "Parser.h"
 #include "Command.h"
